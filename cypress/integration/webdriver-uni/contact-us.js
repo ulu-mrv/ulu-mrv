69c76f82d9295a366a4e82dd92b7ca0b054a127f
Merve <merve.ulug@mtm.org>
2022-04-01 15:44:29 +0200

    neu commit

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -28,12 +28,17 @@ describe("Test contact us form via WebdriverUni", () => {
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
-       
         contact_Us_PO.contactForm_Submission(Cypress.env("first_name"),data.last_name,data.email, "Hallo World", 'h1', 'Thank You for your Message!');
     })
 
     it("Should not be able to submit a successful submition via Contact us form as all fields are required", () => {
+
+        if(Cypress.isBrowser('firefox')) {
+
+        }else {
+            contact_Us_PO.contactForm_Submission(Cypress.env("first_name"),data.last_name," ", "Hallo World", 'body', 'Error: Invalid email address');
+        }
        
-        contact_Us_PO.contactForm_Submission(Cypress.env("first_name"),data.last_name," ", "Hallo World", 'body', 'Error: Invalid email address');
+       
     })
 })
